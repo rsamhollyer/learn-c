7a52eea8b481f0ae1b49005ae2e308945911fecd
Sam Hollyer <rsamhollyer@gmail.com>
2022-12-28 14:38:25 -0600

    updates

---
+++ input/input.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int radius;
+
+    printf("Please enter the radius of the circle: ");
+    scanf("%i", &radius); /* &: Address of operator -> allows mutation */
+
+    double area = 3.14159 * (radius * radius);
+
+    printf("The area of a circle with radius %i is %f\n", radius, area);
+
+    return 0;
+}
