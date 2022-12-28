49da04780dde7b7a94b85aa3e6210a746554ab57
Sam Hollyer <rsamhollyer@gmail.com>
2022-12-28 16:24:09 -0600

    updates

---
+++ numerics/numeric-data-types.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    /* int, double, float */
+
+    int dogs = 2; // whole numbers
+
+    // when you are unsure if the number is a float or a double, err on the side of double
+
+    // doubles and floats are decimal numbers -> doubles is 'double' precision, floats is 'single' precision
+    // doubles take up more memory than floats -> this is why you might use floats instead for some operations
+
+    printf("%i %f %f\n", 1, 11111.1111, 1111.111111F); // Converts the float to a double
+
+    return 0;
+}
