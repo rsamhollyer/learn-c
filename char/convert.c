e328b4cb56e81ecd72b73ee438d3d9bd12c210e2
Sam Hollyer <rsamhollyer@gmail.com>
2022-12-29 15:07:22 -0600

    updates

---
+++ char/convert.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // char toAscii;
+    // printf("Enter a character: ");
+    // scanf("%c", &toAscii);
+
+    // printf("%i\n", toAscii);
+
+    // int toChar;
+    // printf("Enter a number from 0-127: ");
+    // scanf("%i", &toChar);
+
+    // printf("%c\n", (char)toChar);
+
+    // ASCII Math
+
+    char useMath = 'A' + '\t'; // \t -> tab
+
+    printf("A(65) + \\t(11) = %c(%d)\n", useMath, useMath);
+
+    return 0;
+}
