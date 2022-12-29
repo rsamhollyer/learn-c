dbd9157ecd6066afcadf380a33f8f7938f8efa64
Sam Hollyer <rsamhollyer@gmail.com>
2022-12-29 15:10:19 -0600

    updates

---
+++ booleans/examples.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+    _Bool isTrue = 144554; // this number is just arbitrary, as long as it is not 0, it is true
+    _Bool isFalse = 0;
+
+    printf("isTrue: %i, isFalse: %i\n", isTrue, isFalse); // 1, 0
+    return 0;
+}
